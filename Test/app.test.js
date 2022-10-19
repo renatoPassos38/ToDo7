0a08e0b290fd2c211c21a805d10f768c13e850ca
renatoPassos38 <renatogpassos@hotmail.com>
2022-10-18 23:07:40 -0300

    finalizado test da calculadora com sucesso

--- Test/app.test.js
+++ Test/app.test.js
@@ -15,3 +15,19 @@ test("Qual o valor da multiplicar", ()=>{
 test("Qual o valor da dividir", ()=>{
     expect(ServiceConta.dividir(25,5)).toBe(5)
 })
+
+test("Qual o valor da soma", ()=>{
+    expect(ServiceConta.somar(10,10)).toBe(20)
+})
+
+test("Qual o valor da subtrair", ()=>{
+    expect(ServiceConta.subtrair(100,50)).toBe(50)
+})
+
+test("Qual o valor da multiplicar", ()=>{
+    expect(ServiceConta.multiplicar(9,9)).toBe(81)
+})
+
+test("Qual o valor da dividir", ()=>{
+    expect(ServiceConta.dividir(10,2)).toBe(5)
+})
