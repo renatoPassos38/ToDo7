577f59fade66a360c5718bfd2318057b5c7841bc
renatoPassos38 <renatogpassos@hotmail.com>
2022-10-18 23:00:38 -0300

    realizado test multiplicação

--- Test/app.test.js
+++ Test/app.test.js
@@ -8,4 +8,6 @@ test("Qual o valor da subtrair", ()=>{
     expect(ServiceConta.subtrair(10,5)).toBe(5)
 })
 
-
+test("Qual o valor da multiplicar", ()=>{
+    expect(ServiceConta.multiplicar(6,6)).toBe(36)
+})
